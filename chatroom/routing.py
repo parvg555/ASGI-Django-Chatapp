26be819fda89e73c8f5b9b214cf877cd21af73fc
Parv Gupta <parvg555@gmail.com>
2021-05-05 15:42:08 +0530

    chatapp complete

---
+++ chatroom/routing.py
@@ -0,0 +1,13 @@
+import os
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+import chat.routing
+
+
+application = ProtocolTypeRouter({
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    ),
+})
